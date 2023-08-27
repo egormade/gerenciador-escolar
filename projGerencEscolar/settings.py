a56774c1b5d79fc76e9818c64c41f3a99be7feb1
Egor <96543667+egormade@users.noreply.github.com>
2023-08-27 15:06:16 -0300

    database validation

--- projGerencEscolar/settings.py
+++ projGerencEscolar/settings.py
@@ -53,13 +53,14 @@ TEMPLATES = [
 WSGI_APPLICATION = 'projGerencEscolar.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mytestdb',
+        'USER'    : 'test',
+        'PASSWORD': 'mypass123',
+        'HOST'    : 'localhost',
+        'PORT'    : '3306',
     }
 }
 
